19a0b45f99f2093defc36caa0a377c95768454a7
Marwan-Zakia <marwanamir.ma@gmail.com>
2021-06-09 23:11:17 +0300

    html

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// let userName = prompt('Whats your name ?');
-// alert("welcome to my badly stylized site " + userName)
+let userName = prompt('Whats your name ?');
+alert("welcome to my badly stylized site " + userName)
 
 
-// let favColor = prompt('whats your favorite color?');
-// alert('I also like' + favColor)
+let favColor = prompt('whats your favorite color?');
+alert('I also like' + favColor)
 
 
-// let favIcecream = prompt('Whats your favorite icecream?');
-// alert(favIcecream + 'huh intresting')
+let favIcecream = prompt('Whats your favorite icecream?');
+alert(favIcecream + 'huh intresting')
 
 
-// let likePrompts = prompt('do you like prompts ?');
-// alert("i also " + userName)
+let likePrompts = prompt('do you like prompts ?');
+alert("i also " + userName)
 
 
